a1d58e726d4ee1b16fe4ed1b4e36e3e6397f0aaa
Dhanu <dhanushan.info@gmail.com>
2023-02-11 01:48:59 +0530

    firebase commit

--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
