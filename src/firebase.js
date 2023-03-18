9428a766f37d9a44501acef6e6e0527d0888e107
Dhanu <dhanushan.info@gmail.com>
2023-03-18 10:05:29 +0530

    firebase uncommented.

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,22 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-// import { initializeApp } from "firebase/app"
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+// // import { initializeApp } from "firebase/app"
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCmtsWJATm44UMusH-n_ugpeksHwZSIpqk",
-  authDomain: "netflix-clone-6c7d8.firebaseapp.com",
-  projectId: "netflix-clone-6c7d8",
-  storageBucket: "netflix-clone-6c7d8.appspot.com",
-  messagingSenderId: "324562696664",
-  appId: "1:324562696664:web:342c809d8be017ec7ddaad"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCmtsWJATm44UMusH-n_ugpeksHwZSIpqk",
+//   authDomain: "netflix-clone-6c7d8.firebaseapp.com",
+//   projectId: "netflix-clone-6c7d8",
+//   storageBucket: "netflix-clone-6c7d8.appspot.com",
+//   messagingSenderId: "324562696664",
+//   appId: "1:324562696664:web:342c809d8be017ec7ddaad"
+// };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore(); 
-const auth = firebase.auth();
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore(); 
+// const auth = firebase.auth();
 
-export { firebase }
-export { auth };
-export default db;
+// export { firebase }
+// export { auth };
+// export default db;
